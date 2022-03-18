945118a4fa2a82a90448379add64561543ec42e0
Sara Noor <saranoor@wifi-roaming-128-4-143-72.wifi.udel.edu>
2022-03-18 14:18:54 -0400

    fix test

--- src/quizzer/QuizList.test.tsx
+++ src/quizzer/QuizList.test.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { QuizList } from "./QuizList";
-import userEvent from "@testing-library/user-event";
 import quizData from "../quizData/quizData.json";
-import { QUIZZES } from "./Quizzer";
 
 describe("QuizList Tests", () => {
     beforeEach(() => {
